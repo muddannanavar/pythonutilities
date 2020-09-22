e542c27fbdaad8a7ff6331e5dbcd491cd7ebc091
Acharya Muddannanavar Rudrappa <rrudrappa@turvo.com>
2020-09-22 11:33:11 -0500

    updated print

--- dirCompare_withReport.py
+++ dirCompare_withReport.py
@@ -15,7 +15,7 @@ common_list = []
 dir1_only_list = []
 dir2_only_list = []
 
-def print_diff_files(dcmp):
+def get_diff_files(dcmp):
     for name in dcmp.diff_files:
         #print(f"diff file {name} in {dcmp.left} and {dcmp.right}")
         diff_file = {'name':name, 'dir1_path':dcmp.left, 'dir2_path': dcmp.right}
@@ -33,55 +33,53 @@ def print_diff_files(dcmp):
         dir2_only = {'name':name, 'dir2_path':dcmp.right}   
         dir2_only_list.append(dir2_only)
     for sub_dcmp in dcmp.subdirs.values():
-        print_diff_files(sub_dcmp)
+        get_diff_files(sub_dcmp)
+
+def print_header():
+    print ("\nDiff report")
+    print ("="*40)
+    print(f"{'status':12}|{'name':30}|{'dir1_path':40}|{'dir2_path':40}")
+    print ("-"*120)    
+
+def print_info(status="", filename="", dir1="", dir2=""):
+    print(f"{status:12}|{filename:40}|{dir1:40}|{dir2:40}")
+
+def print_diff_report(different_list):
+    with open(DIFF_REPORT, 'w') as f:
+        for i in different_list:
+            l_file = f"{i['dir1_path']}/{i['name']}"
+            r_file = f"{i['dir2_path']}/{i['name']}"
+            diff_cmd = f"diff {l_file} {r_file}"
+            f.write(f"Diff - {l_file} vs {r_file}\n")
+            f.write("="*20 + "\n")
+            try:
+                diff_output=subprocess.check_output(diff_cmd, stderr=subprocess.STDOUT, shell=True)
+            except subprocess.CalledProcessError as e:
+                f.write(e.output.decode("utf-8"))    
+
 
 dcmp = dircmp(DIR_1, DIR_2) 
-print_diff_files(dcmp) 
+get_diff_files(dcmp) 
 
-#different files/subdirectories
-print ("\nDifferent files/sub-directory")
-print ("="*40)
-print(f"{'name':30}|{'dir1_path':40}|{'dir2_path':40}")
-print ("-"*120)
+#print header
+print_header()
 
-#capture diff resutl to DIFF_REPORT file
-with open(DIFF_REPORT, 'w') as f:
-    for i in different_list:
-        #print(f"{i}")
-        print(f"{i['name']:30}|{i['dir1_path']:40}|{i['dir2_path']:40}")
-        l_file = f"{i['dir1_path']}/{i['name']}"
-        r_file = f"{i['dir2_path']}/{i['name']}"
-        diff_cmd = f"diff {l_file} {r_file}"
-        f.write(f"Diff - {l_file} vs {r_file}\n")
-        f.write("="*20 + "\n")
-        try:
-            diff_output=subprocess.check_output(diff_cmd, stderr=subprocess.STDOUT, shell=True)
-        except subprocess.CalledProcessError as e:
-            f.write(e.output.decode("utf-8"))
+# differen files
+for i in different_list:
+    print_info(status="DIFFERENT", filename=i['name'], dir1=i['dir1_path'], dir2=i['dir2_path'])
+
+##just for different files generate deatiled report
+print_diff_report(different_list=different_list)
 
 #common files/subdirectories
-print ("\n\nCommon files/sub-directory")
-print ("="*40)
-print(f"{'name':30}|{'dir1_path':40}|{'dir2_path':40}")
-print ("-"*120)
 for i in common_list:
-    #print(f"{i}")
-    print(f"{i['name']:30}|{i['dir1_path']:40}|{i['dir2_path']:40}")
+    print_info(status="NO-DIFF", filename=i['name'], dir1=i['dir1_path'], dir2=i['dir2_path'])
 
 #only in DIR1
-print ("\n\nOnly in DIR1")
-print ("="*40)
-print(f"{'name':30}|{'dir1_path':40}")
-print ("-"*70)
 for i in dir1_only_list:
-    #print(f"{i}")
-    print(f"{i['name']:30}|{i['dir1_path']:40}")
+    print_info(status="ONLY-DIR1", filename=i['name'], dir1=i['dir1_path'], dir2="")
 
 #only in DIR2
-print ("\nOnly in DIR2")
-print ("="*40)
-print(f"{'name':30}|{'dir2_path':40}")
-print ("-"*70)
 for i in dir2_only_list:
-    #print(f"{i}")
-    print(f"{i['name']:30}|{i['dir2_path']:40}")
+    print_info(status="ONLY-DIR2", filename=i['name'], dir1="", dir2=i['dir2_path'])
+  
